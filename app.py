9e6b544c1a9733e6753669b4cc77a5b34acc04cf
alevventurini <alessandro.venturini@govegans.it>
2024-01-12 11:46:55 +0100

    added persist logic

--- app.py
+++ app.py
@@ -3,14 +3,27 @@ from flask import Flask, request, jsonify
 from langchain_community.document_loaders import DirectoryLoader
 from langchain.chains import ConversationalRetrievalChain
 from langchain.indexes import VectorstoreIndexCreator
+from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI,OpenAIEmbeddings
 from dotenv import load_dotenv
 from langchain_community.vectorstores import Chroma
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 
+
 load_dotenv()
 
 app = Flask(__name__)
+
+template = """Sei Clara, BOT che si occuppa di illustrare il codice etico di Beije.
+Context: {context}
+Chat History: {chat_history}
+Follow Up Input: {question}
+Standalone question:"""
+
+PROMPT = PromptTemplate(
+    input_variables=["context", "chat_history", "question"],
+    template=template
+)
 
 if os.path.exists("persist"):
     print("Reusing index...\n")
@@ -23,6 +36,7 @@ else:
 chain = ConversationalRetrievalChain.from_llm(
     llm=ChatOpenAI(openai_api_key=os.environ.get('OPENAPI_API_KEY'), model="gpt-3.5-turbo"),
     retriever=index.vectorstore.as_retriever(search_kwargs={"k": 2}),
+    combine_docs_chain_kwargs={"prompt": PROMPT}
 )
 
 chat_history = []
